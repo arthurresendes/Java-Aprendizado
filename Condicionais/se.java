5fb4b2ae60f9eda967b9e5d40ed00dd6a7b07319
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-02-17 00:24:57 -0300

    Simple conditional if and else for boolean value

---
+++ Condicionais/se.java
@@ -0,0 +1,12 @@
+package Condicionais;
+
+public class se {
+    public static void main(String[] args){
+        boolean isValid = true;
+        if(isValid){
+            System.out.println("Validado com sucesso");
+        }else{
+            System.out.println("Acesso negado");
+        }
+    }
+}
