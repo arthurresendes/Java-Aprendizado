03234cdef3714f48fe100fc33c2fee1848455d90
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-11 16:51:58 -0300

    Processamento de dados

---
+++ Sequencial/Dados/Processamento.java
@@ -0,0 +1,11 @@
+public class Processamento {
+    public static void main(String[] args) {
+        float base_maior = 9.5f;
+        float base_menor = 5.0f;
+        float altura = 12.2f;
+        float area = (base_maior + base_menor) * altura / 2;
+
+        System.out.printf("Area: %.2f%n", area);
+
+    }
+}
