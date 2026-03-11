4889fcf4dcf764ee092ee9965d5144c80414a92a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-11 16:51:06 -0300

    Saida de dados

---
+++ Sequencial/Dados/Saida.java
@@ -0,0 +1,14 @@
+public class Saida
+{
+public static void main(String[] args) {
+String nome = "Arthur";
+int idade = 19;
+double salario = 9999.99;
+
+System.out.println(nome + " tem " + idade  + " anos e com o salario de RS " + salario + " reais");
+
+
+// Usamos printf quando especificamos com % os casos
+System.out.printf("%s tem %d anos e com o salario de RS %.2f reais %n", nome, idade, salario);
+}
+}
