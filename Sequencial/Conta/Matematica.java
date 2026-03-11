ec46b0c2b235c292fb11de3a1f35b0e5d4a05b6f
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-11 16:53:59 -0300

    Operações matematicas

---
+++ Sequencial/Conta/Matematica.java
@@ -0,0 +1,19 @@
+package Sequencial.Conta;
+
+public class Matematica {
+    public static void main(String[] args) {
+        double x = 3.0;
+        double y = 4.0;
+        double z = 5.0;
+        double negative = -10.0;
+
+        double raizQuadrada = Math.sqrt(y);
+        double elevado = Math.pow(x, z);
+        double absoluto = Math.abs(negative);
+
+        System.out.println(raizQuadrada);
+        System.out.println(elevado);
+        System.out.println(absoluto);
+
+    }
+}
