0739a25820f78b73a227b98eca282782fe9df1cb
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-25 16:50:13 -0300

    Lit with your operations in java

---
+++ Lista/Aprendizado/Main.java
@@ -0,0 +1,27 @@
+package Lista.Aprendizado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+
+        lista.add("Ola");
+        lista.add(0,"Hello");
+
+        System.out.println(lista.get(0));
+        System.out.println(lista.get(1));
+
+        lista.set(1,"Ola mundo");
+
+        System.out.println(lista.get(0));
+        System.out.println(lista.get(1));
+
+        System.out.println(lista.size());
+
+        //System.out.println(lista.contains());
+        System.out.println("Lista esta vazia: " + lista.isEmpty());
+
+    }
+}
