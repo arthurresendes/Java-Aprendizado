c72503786ea355e2a472f9c67ecce3feeca77131
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-22 16:50:25 -0300

    SRP - Single Responsability (Separação de responsabilidades)

---
+++ SOLID/Mini_Exemplo/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Notify notificar = new Notify();
+        System.out.println("Ola mundo");
+        notificar.notifyClient();
+    }
+}
---
+++ SOLID/Mini_Exemplo/utility/Notify.java
@@ -0,0 +1,5 @@
+public class Notify {
+    public void notifyClient(){
+        System.out.println("Cliente avisado");
+    }
+}
