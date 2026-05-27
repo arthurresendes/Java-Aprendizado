21d93ea8179e7f2ec2fb4ba4ff2699b5c251d26d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-05-27 19:14:25 -0300

    Strategy

--- ComportamentalPatterns/Pratica/strategy/Main.java
+++ ComportamentalPatterns/Pratica/strategy/Main.java
@@ -1,11 +1,7 @@
 public class Main {
-
     public static void main(String[] args) {
-
         Pagamento pagamento = new Pix();
-
         Loja loja = new Loja(pagamento);
-
         loja.finalizarCompra();
     }
 }
